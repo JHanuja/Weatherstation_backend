1950ca406b707de0c350103d7bb49060ed1fd9ac
Johannes <hanujajohannes@gmail.com>
2022-04-28 15:50:39 +0200

    allow cross domain

--- index.js
+++ index.js
@@ -5,6 +5,17 @@ const pool = require("./db");
 
 app.use(express.json());
 
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', "*");
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+};
+
+app.use(allowCrossDomain);
+
+
+
 
 //post request
 
