9619c592bad6e4f2114304324aec666ed4ddd798
Johannes <hanujajohannes@gmail.com>
2022-05-30 13:29:51 +0200

    json answer

--- index.js
+++ index.js
@@ -26,11 +26,15 @@ app.post("/wsdata",async(req,res) => {
     try {
         let tmp = req.body.temperature;
         let hum = req.body.humidity;
+        let airp = req.body.airPressure;
+        let soilh = req.body.soilHumidity;
+        let gasv = req.body.gasValue;
+        let lightv = req.body.lightValue;
 
         console.log(req.body);
         
         console.log(`${tmp}`);
-        const newWsData = await pool.query(`INSERT INTO dht (temperature,humidity) VALUES ('${tmp}','${hum}')`);
+        const newWsData = await pool.query(`INSERT INTO dht (temperature,humidity,soilhumidity,airpressure,airquality,brightness) VALUES ('${tmp}','${hum}','${soilh}','${airp}','${gasv}','${lightv}')`);
         res.send("OK");
 
         console.log("success");
@@ -40,18 +44,99 @@ app.post("/wsdata",async(req,res) => {
 });
 
 
+/*
+<=now zu < now
+sunrise -1 weg
+
+*/
+
 app.get("/dht", async(req,res)=>{
 
     try{
-        const record = await pool.query(`SELECT daytime, temperature, humidity
+        const singleValues = await pool.query(`SELECT daytime, temperature, humidity,airquality,airpressure
         FROM dht
         ORDER BY daytime DESC
         LIMIT 1`);
 
-        res.json(record.rows[0]);
+        let temperature = singleValues.rows[0].temperature;
+        let daytime = singleValues.rows[0].daytime;
+        let humidity = singleValues.rows[0].humidity;
+        let airquality = singleValues.rows[0].airquality;
+        let airpressure = singleValues.rows[0].airpressure;
+
+        const temperatureValues = await pool.query(`Select Date(daytime), Max(temperature) As maxtemp ,Avg(temperature) As avgtemp, Min(temperature) as mintemp,  Avg(humidity) As avghum, Avg(soilhumidity) As avgsoilhum 
+                                                    From (Select * From dht 
+                                                    Where  Date(daytime) <= now()::DATE AND Date(daytime) > (now()::DATE -8)) as Week Group By Date(daytime);
+
+        `);
+
+        let mintemp = [];
+        let maxtemp = [];
+        let avgtemp = [];
+        let avghum = [];
+        let avgsoilhum = [];
+
+        for (i= 0; i < temperatureValues.rowCount; i ++){
+            mintemp[i] = temperatureValues.rows[i].mintemp;
+            maxtemp[i] = temperatureValues.rows[i].maxtemp;
+            avgtemp[i] = temperatureValues.rows[i].avgtemp;
+            avghum[i] = temperatureValues.rows[i].avghum;
+            avgsoilhum[i] = temperatureValues.rows[i].avgsoilhum;
+
+        }
+        console.log(temperatureValues.rows[0].maxtemp);
+
+        const sun = await pool.query(   `Select Min(daytime) As sunrise,Max(daytime) As sunset From (Select * 
+                                        From dht 
+                                        Where  Date(daytime) = now()::DATE) as Yesterday Group By brightness Having brightness > 0;
+                                    `);
+
+        let sunrise = sun.rows[0].sunrise;
+        let sunset = sun.rows[0].sunset; 
+        
+        const air = await pool.query(`Select Date(daytime) As time, airpressure As airp, airquality as airq From dht WHERE  daytime >= now()- interval '7 hours';`);
+
+        let airpressurehistory = [];
+        let airqualityhistory = [];
+
+        for (i= 0; i < air.rowCount; i ++){
+            let time = air.rows[i].time;
+            let airp = air.rows[i].airp;
+            let airq = air.rows[i].airq;
+
+            let airpjs = {
+                "time": time,
+                "airp": airp
+            }
+
+            let airqjs = {
+                "time": time,
+                "airq": airq
+            }
+
+            airpressurehistory[i] = airpjs;
+            airqualityhistory[i] = airqjs;
+        }
+
+        res.json({
+            "daytime" : daytime,
+            "temperature" : temperature,
+            "humidity": humidity,
+            "airquality": airquality,
+            "airpressure": airpressure,
+            "maxtemp": maxtemp,
+            "mintemp": mintemp,
+            "avgtemp": avgtemp,
+            "avghum": avghum,
+            "avgsoilhum": avgsoilhum,
+            "sunrise": sunrise,
+            "sunset": sunset,
+            "airpressurehistory": airpressurehistory,
+            "airqualityhistory":airqualityhistory
+        });
 
     }catch(err){
-        console.err(err.message);
+        console.error(err.message);
     }
 
 
---
+++ schema.sql
@@ -0,0 +1,39 @@
+Drop table dht;
+
+
+CREATE TABLE dht(
+    dht_id SERIAL PRIMARY KEY, 
+    daytime TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP, 
+    temperature FLOAT NOT NULL, 
+    humidity FLOAT NOT NULL,
+    soilhumidity FLOAT NOT NULL,
+    airpressure FLOAT NOT NULL,
+    airquality FLOAT NOT NULL,
+    brightness FLOAT NOT NULL
+);
+
+Insert into dht (temperature,humidity,soilhumidity,airpressure,airquality,brightness) Values (10.0,10.0,10.0,10.0,10.0,0);
+
+//Gestern anfangen jetzt heute
+Select Date(daytime), Max(temperature) As maxtemp ,Avg(temperature) As avgtemp, Min(temperature) as mintemp,  Avg(humidity) As avghum, Avg(soilhumidity) As soilhum From (Select * From dht Where  Date(daytime) <= now()::DATE AND Date(daytime) > (now()::DATE -8)) as Week Group By Date(daytime);
+
+Insert into dht (temperature,humidity,soilhumidity,airpressure,airquality,brightness) Values (15.0,15.0,15.0,15.0,15.0,0.0);
+
+Insert into dht (temperature,humidity,soilhumidity,airpressure,airquality,brightness) Values (15.0,15.0,15.0,15.0,15.0,1.0);
+
+Select Min(daytime) As sunrise,Max(daytime) As sunset From (Select * From dht Where  Date(daytime) = now()::DATE) as Yesterday Group By brightness Having brightness > 0;
+
+Select Date(daytime), airpressure As airp From dht WHERE  daytime >= now()- interval '7 hours';
+
+Select Date(daytime), airquality As airq From dht WHERE  daytime >= now()- interval '7 hours';
+
+Select Date(daytime), Avg(humidity) As avghum From (Select * From dht Where  Date(daytime) < now()::DATE AND Date(daytime) > (now()::DATE -8)) as Week Group By Date(daytime);
+
+Select Date(daytime), Avg(soilhumidity) As soilhum From (Select * From dht Where  Date(daytime) < now()::DATE AND Date(daytime) > (now()::DATE -8)) as Week Group By Date(daytime);
+
+
+
+SELECT daytime, temperature, humidity,airpressure, soilhumidity, airquality
+        FROM dht
+        ORDER BY daytime DESC
+        LIMIT 1;
